d1e59e39b79958a3111ef6544076a766d6afd13f
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-05-04 19:53:57 -0300

    Revert "styles(components): add Text component"
    This reverts commit 08468bebbb866676c3895a4608c66bf60e03883e.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { Text } from './components';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Text>fefe</Text>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
